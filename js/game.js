55145f9dd0b0fc7f8cecd69a1860dba66edbfc53
aquino062 <willian_wjunior@hotmail.com>
2023-03-25 17:52:19 -0300

    ..

--- js/game.js
+++ js/game.js
@@ -75,7 +75,7 @@ const createCard = (characters) => {
     const front = createElement("div", "face front");
     const back = createElement("div", "face back");
 
-    front.style.backgroundImage = `url('/images/${characters}.png')`;
+    front.style.backgroundImage = `url('../images/${characters}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
